43a4fa38371069d1d53590969a2ee3669a6b4444
Pablo Galindo Roldan <pablogalindo90@gmail.com>
2025-11-03 09:02:06 -0300

    configurada db

--- BACKEND/src/ar/edu/unrn/seminario/accesos/DBConnection.java
+++ BACKEND/src/ar/edu/unrn/seminario/accesos/DBConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DBConnection {
     // Update these constants to match your local database configuration
-    private static final String URL = "jdbc:mysql://localhost:3306/seminario_2024_1?serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/seminario?serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = ""; // set your DB password
+    private static final String PASSWORD = "lila2022"; // set your DB password
 
     static {
         try {
